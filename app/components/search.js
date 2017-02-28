dee62d2caf78129949a6045e2469897af0448914
J. Motyl <webdev@stupiddog.net>
2017-02-28 17:34:36 -0600

    WIP, search form - not rendering fields or button

--- app/components/search.js
+++ app/components/search.js
@@ -1,80 +1,112 @@
 var React = require('react');
 
+var ITEMQTYCHOICES = ['1', '5', '10', '15', '20'];
+
 var Search = React.createClass({
 
-    render: function() {
-        return(
-
-    <div className="row">
-		<div className="col-sm-12">
-		<br />
-			<div className="panel panel-primary">
-				<div className="panel-heading">
-					<h3 className="panel-title"><strong><i className="fa fa-list-alt"></i>    Search Parameters</strong></h3>
-				</div>
-				<div className="panel-body">
-					<form role="form">
-                        <div className="form-group">
-                            <label htmlFor="search">Search Term:</label>
-                            <input className="form-control" id="searchTerm" value="dogs" type="text" />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="pwd">Number of Records to Retrieve:</label>
-                            <select className="form-control" id="numRecordsSelect">
-                                <option value="1">1</option>
-                                <option value="5" selected="selected">5</option>
-                                <option value="10">10</option>
-                            </select>			  
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="startYear">Start Year (Optional):</label>
-                            <input className="form-control" id="startYear" value="2016" type="text" />
-                        </div>
-
-                        <div className="form-group">
-                            <label htmlFor="endYear">End Year (Optional):</label>
-                            <input className="form-control" id="endYear" value="2017" type="text" />
-                        </div>
-
-                        <button type="submit" className="btn btn-default" id="runSearch"><i className="fa fa-search"></i> Search</button>
-                        <button type="button" className="btn btn-default" id="clearAll"><i className="fa fa-trash"></i> Clear Results</button>
-
-					</form>
-				</div>
-			</div>
-		</div>
-    </div>
-
-        );
-    }
-
-/*
     getInitialState: function() {
-        return {counter: 0};
+        return {errors: {}}
     },
 
+    isValid: function() {
+        var fields = ['searchTerm', 'numRecordsSelect', 'startYear', 'endYear']
 
-    handleClick: function(event) {
-        this.setState({
-            counter: this.state.counter + 1
-        });
+        var errors = {}
+        fields.forEach(function(field) {
+            var value = trim(this.refs[field].getDOMNode().value)
+            if(!value) {
+                errors[field] = 'This field is required'
+            }
+        }.bind(this))
+        this.setState({errors: errors})
+    
+        var isValid = true
+        for (var error in errors) {
+            isValid = false
+            break
+        }
+        return isValid
+    },
+
+    getFormData: function() {
+        var data = {
+        }
+        return data
     },
 
     render: function() {
-        var divStyle = {border: "1px solid red"};
         return(
-            <div style={divStyle}>
-                I am a student {this.props.name} | {this.props.age} | {this.props.occupation}
-                <button onClick={this.handleClick}>Click counter {this.state.counter}</button>
-                <hr/>
-                <PersonalStatement id={this.props.name} />
+            <div className="row">
+                <div className="col-sm-12">
+                <br />
+                    <div className="panel panel-primary">
+                        <div className="panel-heading">
+                            <h3 className="panel-title"><strong><i className="fa fa-list-alt"></i>    Search Parameters</strong></h3>
+                        </div>
+                        <div className="panel-body">
+                            <form role="form">
+                                {this.renderTextInput('searchTerm', 'Search Term:')}
+                                {this.renderSelect('numRecordsSelect', 'Number of Records to Retrieve:', ITEMQTYCHOICES)}
+                                {this.renderTextInput('searchTerm', 'Start Date (Optional):')}
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
-        );
+        )
+    },
+
+    renderTextInput: function(id, label) {
+        return this.renderField(id, label,
+            <input type="text" className="form-control" id={id} ref={id}/>
+        )
+    },
+
+    renderSelect: function(id, label, values) {
+        var options = values.map(function(value) {
+            return <option key={value} value={value}>{value}</option>
+        })
+        return this.renderField(id, label,
+            <select className="form-control" id={id} ref={id}>
+                {options}
+            </select>
+        )
+    },
+
+    renderField: function(id, label, field) {
+        return (
+            <div className={$c('form-group', {'has-error': id in this.state.errors})}>
+                <label htmlFor={id}>{label}</label>
+                {field}
+            </div>
+        )
     }
-*/
+
 });
+
+var trim = function() {
+    var TRIM_RE = /^\s+|\s+$/g
+    return function trim(string) {
+        return string.replace(TRIM_RE, '')
+    }
+}()
+
+function $c(staticClassName, conditionalClassNames) {
+    var classNames = []
+    if (typeof conditionalClassNames == 'undefined') {
+        conditionalClassNames = staticClassName
+    }
+    else {
+        classNames.push(staticClassName)
+    }
+    for (var className in conditionalClassNames) {
+        if (!!conditionalClassNames[className]) {
+            classNames.push(className)
+        }
+    }
+    return classNames.join(' ')
+}
+
 
 module.exports = Search;
 
