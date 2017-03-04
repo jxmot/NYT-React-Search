a3dbbd3ec8cf1a9f7907dc76f575753f78e32c62
J. Motyl <webdev@stupiddog.net>
2017-03-04 01:24:57 -0600

    removed unecessary code

--- appSocket.js
+++ appSocket.js
@@ -1,29 +1,14 @@
-
-
+/* ************************************************************************ */
+/*
+    Socket IO Stuff - This is where we'll wait for socket.io events. Any 
+    other module that has access to server.js:app can also wait for events.
+*/
 exports = module.exports = function (io) {
-
     // Set socket.io listeners.
-    io.on('connection', (socket) => {
-
+    io.on('connection', function(socket) {
         console.log('client connected');
-        //console.log(socket);
     
-        // On conversation entry, join broadcast channel
-        socket.on('enter conversation', (conversation) => {
-            socket.join(conversation);
-            console.log('joined ' + conversation);
-        });
-    
-        socket.on('leave conversation', (conversation) => {
-            socket.leave(conversation);
-            console.log('left ' + conversation);
-        });
-    
-        socket.on('new message', (conversation) => {
-            io.sockets.in(conversation).emit('refresh messages', conversation);
-        });
-    
-        socket.on('disconnect', () => {
+        socket.on('disconnect', function()= {
             console.log('client disconnected');
         });
     });
