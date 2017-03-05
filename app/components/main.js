f83463a91ea8236730dfc4ccda1ed5055be85e7e
J. Motyl <webdev@stupiddog.net>
2017-03-04 23:18:47 -0600

    minor changes

--- app/components/main.js
+++ app/components/main.js
@@ -9,16 +9,16 @@ var Search = require('./search.js');
 
 var Main = React.createClass({
     render: function render() {
-        var jumboStyle = {backgroundColor: "#20315A", color: "white", 'margin-top': "10px"};
-        var linkStyle = {'font-size': "small"}
+        var jumboStyle = {backgroundColor: "#20315A", color: "white", marginTop: "10px"};
+        var linkStyle = {fontSize: "small"}
         return(
             <div>
                 <div className="jumbotron" style={jumboStyle}>
                     <h2 className="text-center"><strong><i className="fa fa-newspaper-o"></i>    New York Times Search</strong></h2>
-                    <h4 className="text-center">A Node, Express, React, Mongoose, Socket.io Application</h4>
                 </div>
-                <div>
-                        <a className="btn btn-link text-center" style={linkStyle} target="_blank" href="https://github.com/jxmot/NYT-React-Search">Github Repository</a>
+                <div className="text-center">
+                    <h4>A Node, Express, React, Mongoose, Socket.io Application</h4>
+                    <a className="btn btn-link" style={linkStyle} target="_blank" href="https://github.com/jxmot/NYT-React-Search">Github Repository</a>
                 </div>
                 <Search />
             </div>
